052ea78050b19c1b61504d96c8d8f763e4a5c819
chenqikun <qikunchen@foxemail.com>
2019-08-20 19:35:18 +0800

    李四修改

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,7 +5,8 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
-System.out.println("修改后的代码");
-
+		System.out.println("修改后的代码");
+		System.out.println("李四修改后的代码");
+		
     }
 }
